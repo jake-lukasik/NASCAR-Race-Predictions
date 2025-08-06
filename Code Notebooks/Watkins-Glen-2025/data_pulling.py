e25f1beab24b9c284014bed2b87e880a08e8302f
jake-lukasik <146891855+jake-lukasik@users.noreply.github.com>
2025-08-06 19:59:58 -0400

    quick

---
+++ Code Notebooks/Watkins-Glen-2025/data_pulling.py
@@ -0,0 +1,100 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from io import StringIO
+
+def pull_data_from_driver_averages(url, driver_in_first, driver_in_fourth):
+    # send the HTTP request
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+
+    # parse the HTML of the webpage
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # find all <table> html objects
+    tables = soup.find_all("table")
+
+    # check how many tables there are (how many we'll have to iterate through in the next cell)
+    print(f"Found {len(tables)} tables.")
+
+    # loop through found tables and save the correct one as a pandas df
+    for i in range(len(tables)):
+        try:
+            html_str = str(tables[i])
+            temp = pd.read_html(StringIO(html_str))[0]
+            if "Driver" in temp.columns:
+                if temp.iloc[0]["Driver"] == driver_in_first and temp.iloc[3]["Driver"] == driver_in_fourth: ## change this line to the best driver as of recent in the table
+                    track_stats = temp
+                    break
+        except Exception as e:
+            print(f"Skipping table {i}: {e}")
+
+    track_stats.columns = track_stats.columns.str.replace("  ", " ", regex=False).str.strip()
+    for col in track_stats.columns:
+        if "Unnamed" in col:
+            track_stats = track_stats.drop(columns=[col])
+    
+    return track_stats
+
+def pull_season_stats(url="https://www.driveraverages.com/nascar/nascar-stats.php", driver_in_first=""):
+    # send the HTTP request
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+
+    # parse the HTML of the webpage
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # find all <table> html objects
+    tables = soup.find_all("table")
+
+    for i in range(len(tables)):
+        try:
+            html_str = str(tables[i])
+            temp = pd.read_html(StringIO(html_str))[0]
+            if "Driver" in temp.columns:
+                if temp.iloc[0]["Driver"] == driver_in_first: ## change this line to the best driver as of recent in the season points
+                    season_stats = temp
+                    break
+        except Exception as e:
+            print(f"Skipping table {i}: {e}")
+
+    season_stats.columns = season_stats.columns.str.replace("  ", " ", regex=False).str.strip()
+    season_stats = season_stats.drop(columns=["NASCAR Points", "Top 10's"])
+    for col in season_stats.columns:
+        if "Unnamed" in col:
+            season_stats = season_stats.drop(columns=[col])
+
+    season_stats = season_stats.rename(
+        columns={col: f"szn_{col}" for col in season_stats.columns if col != "Driver"}
+    )
+
+    return season_stats
+
+def pull_road_course_stats(url="https://www.driveraverages.com/nascar/tracktype_avg.php?trktype_id=4", driver_in_first=""):
+    # send the HTTP request
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+
+    # parse the HTML of the webpage
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # find all <table> html objects
+    tables = soup.find_all("table")
+
+    for i in range(len(tables)):
+        try:
+            html_str = str(tables[i])
+            temp = pd.read_html(StringIO(html_str))[0]
+            if "Driver" in temp.columns:
+                if temp.iloc[0]["Driver"] == driver_in_first: ## change this line to the best driver as of recent in the season points
+                    rd_course_stats = temp
+                    break
+        except Exception as e:
+            print(f"Skipping table {i}: {e}")
+
+    rd_course_stats.columns = rd_course_stats.columns.str.replace("  ", " ", regex=False).str.strip()
+    for col in rd_course_stats.columns:
+        if "Unnamed" in col:
+            rd_course_stats = rd_course_stats.drop(columns=[col])
+
+    return rd_course_stats
